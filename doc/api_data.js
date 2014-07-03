2f2540e0266d32bb55b338919eceee17f38edad8
guiguiabloc <guiguiabloc@gmail.com>
2014-07-03 19:28:38 +0200

    Doc for Tempo

--- doc/api_data.js
+++ doc/api_data.js
@@ -57,5 +57,57 @@ define({ api: [
     ],
     "version": "0.0.0",
     "filename": "./apidomogeek.py"
+  },
+  {
+    "type": "get",
+    "url": "/tempoedf/:date",
+    "title": "Tempo EDF color Request",
+    "name": "GetTempo",
+    "group": "Api.domogeek.fr",
+    "description": "Ask the EDF Tempo color",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "field": "now",
+            "optional": false,
+            "description": "Ask for today."
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "field": "tomorrow",
+            "optional": false,
+            "description": "Ask for tomorrow."
+          }
+        ]
+      }
+    },
+    "success": {
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "HTTP/1.1 200 OK\nContent-Type: application/json\nTransfer-Encoding: chunked\nDate: Thu, 03 Jul 2014 17:16:47 GMT\nServer: localhost\n{\"tempocolor\": \"bleu\"}\n"
+        }
+      ]
+    },
+    "error": {
+      "examples": [
+        {
+          "title": "Error-Response:",
+          "content": "HTTP/1.1 400 Bad Request\n400 Bad Request\n"
+        }
+      ]
+    },
+    "examples": [
+      {
+        "title": "Example usage:",
+        "content": "   curl http://api.domogeek.fr/tempoedf/now\n   curl http://api.domogeek.fr/tempoedf/tomorrow\n"
+      }
+    ],
+    "version": "0.0.0",
+    "filename": "./apidomogeek.py"
   }
 ] });
