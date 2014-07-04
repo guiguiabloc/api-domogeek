043158988ee65039923cb7378961a366a741c433
guiguiabloc <guiguiabloc@gmail.com>
2014-07-04 23:22:30 +0200

    add package.json

--- doc/api_project.js
+++ doc/api_project.js
@@ -1,6 +1,9 @@
 define({
+  "name": "API DomoGeek",
+  "version": "0.1.0",
+  "description": "DomoGeek API Documentation",
   "generator": {
     "version": "0.5.0",
-    "time": "2014-07-04T21:15:44.960Z"
+    "time": "2014-07-04T21:22:24.907Z"
   }
 });
