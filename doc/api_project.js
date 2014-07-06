8ac2b4d4b53e2fe2d80e2c34f7343113069bcd3e
guiguiabloc <guiguiabloc@gmail.com>
2014-07-06 12:57:55 +0200

    Doc school calendar

--- doc/api_project.js
+++ doc/api_project.js
@@ -4,6 +4,6 @@ define({
   "description": "DomoGeek API Documentation",
   "generator": {
     "version": "0.5.0",
-    "time": "2014-07-04T21:22:24.907Z"
+    "time": "2014-07-06T10:57:49.137Z"
   }
 });
