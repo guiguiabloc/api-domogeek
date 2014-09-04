7c4144ff9a6a2b4ba635cc9c358a2bc110cf048f
guiguiabloc <guiguiabloc@gmail.com>
2014-09-04 11:19:56 +0200

    Add Control

--- apidomogeek.py
+++ apidomogeek.py
@@ -545,6 +545,8 @@ class vigilance:
         format = request[2]
       except:
         format = None
+      if len(dep) > 2:
+        return "Incorrect request : /vigilance/{department number}/{color|risk|flood|all}\n"
       if vigilancequery not in ["color","risk","flood", "all"]: 
         return "Incorrect request : /vigilance/{department}/{color|risk|flood|all}\n"
       result = vigilancerequest.getvigilance(dep)
