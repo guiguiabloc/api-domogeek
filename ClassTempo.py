0faf2c9b17b8b268ec43e5836b3166d42a57db40
guiguiabloc <gur@Thanatos.styx.net>
2015-11-08 12:52:00 +0100

    Modification EDF Tempo

--- ClassTempo.py
+++ ClassTempo.py
@@ -9,29 +9,33 @@
 import urllib2
 import sys
 import json
+import datetime
+from datetime import timedelta
 
 class EDFTempo:
   def TempoToday(self):
+    now = datetime.datetime.now()
+    today= now.strftime("%Y-%m-%d")
     try:
-      page = urllib2.urlopen('https://particulier.edf.fr/bin/edf_rc/servlets/ejptempo?searchType=tempo')
+      page = urllib2.urlopen('https://particulier.edf.fr/bin/edf_rc/servlets/ejptemponew?Date_a_remonter='+today+'&TypeAlerte=TEMPO')
     except:
       return None
     response = json.load(page)
-    data=response["data"]
-    colortoday = data.split('"')[9]
+    colortoday = response['JourJ']['Tempo']
     return colortoday
 
   def TempoTomorrow(self):
+    now = datetime.datetime.now()
+    datetomorrow = now + timedelta(days=1)
+    tomorrow = datetomorrow.strftime("%Y-%m-%d")
     try:
-      page = urllib2.urlopen('https://particulier.edf.fr/bin/edf_rc/servlets/ejptempo?searchType=tempo')
+      page = urllib2.urlopen('https://particulier.edf.fr/bin/edf_rc/servlets/ejptemponew?Date_a_remonter='+tomorrow+'&TypeAlerte=TEMPO')
     except:
       return None
     response = json.load(page)
-    data=response["data"]
     try:
-      colortomorrow = data.split('"')[17]
+      colortomorrow = response['JourJ']['Tempo']
       return colortomorrow
     except:
       return "no color"
-
 
